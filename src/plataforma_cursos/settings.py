2c176940593aa600cf339889edb1aff6ca149a8f
jonathan Oliveira <jonathanjorge8445@gmail.com>
2021-10-05 22:19:17 -0300

    :wrench: add docker

--- src/plataforma_cursos/settings.py
+++ src/plataforma_cursos/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vh7kcatpqdk!i2$ke&5rkcc$j8iec^zbx+f9&49uflb&e59zez'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
