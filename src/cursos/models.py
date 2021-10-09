871ed56ad4b64927cb1d78c52bcb24d893a483be
Jonathan Oliveira <jonathan.jb.oliveira@gmail.com>
2021-10-09 15:38:48 -0300

    :hammer: add comment and rate

--- src/cursos/models.py
+++ src/cursos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from usuarios.models import Usuario
+from datetime import datetime
 
 
 class Curso(models.Model):
@@ -18,3 +20,27 @@ class Aula(models.Model):
 
     def __str__(self) -> str:
         return self.nome
+
+
+class Comentario(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
+    comentario = models.TextField()
+    data = models.DateTimeField(default=datetime.now)
+    aula = models.ForeignKey(Aula, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.usuario.nome
+
+
+class NotaAula(models.Model):
+    choices = (
+        ('p', 'Péssimo'),
+        ('r', 'Ruim'),
+        ('re', 'Regular'),
+        ('b', 'bom'),
+        ('o', 'Ótimo')
+    )
+
+    aula = models.ForeignKey(Aula, on_delete=models.DO_NOTHING)
+    nota = models.CharField(max_length=50, choices=choices)
+    usuario = models.ForeignKey(Usuario, on_delete=models.DO_NOTHING)
